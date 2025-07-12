b56b4796075e610fba912089f2fef39e1a3f59c0
Jack Day <jacktday@gmail.com>
2025-07-12 13:54:34 +1000

    Fix as Boltz assumes that AAs have N, CA, C as the first 3 atoms

--- add_mol_to_ccd.py
+++ add_mol_to_ccd.py
@@ -11,6 +11,8 @@ import argparse
 from CifFile import ReadCif
 import yaml
 import copy
+import itertools
+import operator
 
 def extractConstraints(constraints, key, transpose=False):
     result = np.array(list(
@@ -37,18 +39,20 @@ def getPickledProp(mol, name):
         bytes.fromhex(mol.GetProp(name))
     )
 
-def backbone_from_smarts(smarts: str, atom_names: list[str | None], leaving_atoms: set[str]) -> Chem.Mol:
+def backbone_from_smarts(smarts: str, atom_names: list[str | None], atom_idxs: list[int | None] | None, leaving_atoms: set[str]) -> Chem.Mol:
     mol = Chem.MolFromSmarts(smarts)
 
     if mol.GetNumAtoms() != len(atom_names):
         raise Exception("Backbone atom names do not match smarts!")
     
-    for atom_name, atom in zip(atom_names, mol.GetAtoms()):
+    for atom_name, atom_idx, atom in zip(atom_names, atom_idxs or itertools.repeat(None), mol.GetAtoms()):
         if atom_name:
             atom.SetProp("name", atom_name)
             residueInfo = atom.GetPDBResidueInfo() or Chem.AtomPDBResidueInfo()
             residueInfo.SetName(atom_name)
             atom.SetPDBResidueInfo(residueInfo)
+            if atom_idx != None:
+                atom.SetIntProp("idx", atom_idx)
         
         atom.SetIntProp("leaving_atom", int(atom_name in leaving_atoms))
 
@@ -58,16 +62,20 @@ default_backbones = {
     "protein": backbone_from_smarts(
         "[NX3][CX4][CX3](=O)[O]",
         ["N", "CA", "C", "O", "OXT"],
+        [0, 1, 2, 3, -1],
         set(["OXT"])
+        
     ),
     "RNA": backbone_from_smarts(
         "[O][PX4](=O)([O])[OX2][CX4][CX4]([OX2]1)[CX4]([OX2])[CX4]([OX2])[CX4]1",
         ["OP3", "P", "OP1", "OP2", "O5'", "C5'", "C4'", "O4'", "C3'", "O3'", "C2'", "O2'", "C1'"],
+        None,
         set(["OP3"])
     ),
     "DNA": backbone_from_smarts(
         "[O][PX4](=O)([O])[OX2][CX4][CX4]([OX2]1)[CX4]([OX2])[CX4][CX4]1",
         ["OP3", "P", "OP1", "OP2", "O5'", "C5'", "C4'", "O4'", "C3'", "O3'", "C2'", "C1'"],
+        None,
         set(["OP3"])
     ),
 }
@@ -80,7 +88,7 @@ def getBackbone(mol: Chem.Mol, backbones: list[Chem.Mol]):
 
     return None, None
 
-def addBackboneProps(mol: Chem.Mol, backbones: list[Chem.Mol], props: set[str] = set(["name", "leaving_atom"])):
+def addBackboneProps(mol: Chem.Mol, backbones: list[Chem.Mol], props: set[str] = set(["name", "leaving_atom", "idx"])):
     backbone, indices = getBackbone(mol, backbones)
 
     if not backbone:
@@ -101,6 +109,30 @@ def addBackboneProps(mol: Chem.Mol, backbones: list[Chem.Mol], props: set[str] =
 
             value = backbone_atom.GetProp(prop)
             mol_atom.SetProp(prop, value)
+            
+def renumberAtoms(mol: Chem.Mol) -> Chem.Mol:
+    start_atoms = {}
+    other_atoms = []
+    end_atoms = {}
+
+    for atom in mol.GetAtoms():
+        if not atom.HasProp("idx"):
+            other_atoms.append(atom.GetIdx())
+            continue
+
+        idx = atom.GetIntProp("idx")
+        if idx >= 0:
+            start_atoms[idx] = atom.GetIdx()
+        else:
+            end_atoms[idx] = atom.GetIdx()
+    
+    new_atom_order = list(itertools.chain(
+        map(operator.itemgetter(1), sorted(start_atoms.items())),
+        other_atoms,
+        map(operator.itemgetter(1), sorted(end_atoms.items()))
+    ))
+
+    return Chem.RenumberAtoms(mol, new_atom_order)
 
 def addBoltzParams(resname: str, mol: Chem.Mol, parse_ccd_residue):
     # Add missing atom names
@@ -236,6 +268,7 @@ def boltzMolFromSmiles(resname: str, smiles: str, backbones: list[Chem.Mol]) -> 
 
     addBackboneProps(mol, backbones)
     mol = AllChem.RemoveHs(mol, sanitize=False)
+    mol = renumberAtoms(mol)
     addBoltzParams(resname, mol, schema_parse_ccd_residue)
     return mol
 
@@ -262,6 +295,7 @@ def boltzMolFromPDB(resname: str, filename: str | Path, backbones: list[Chem.Mol
 
     addBackboneProps(mol, backbones)
     mol = AllChem.RemoveHs(mol, sanitize=False)
+    mol = renumberAtoms(mol)
     addBoltzParams(resname, mol, schema_parse_ccd_residue)
     return mol
 
@@ -320,6 +354,7 @@ def main():
             backbones[backbone_name] = backbone_from_smarts(
                 backbone_template["smarts"],
                 backbone_template["atom_names"],
+                backbone_template["atom_idxs"],
                 backbone_template["leaving_atoms"],
             )
 
